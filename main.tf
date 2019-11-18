4d0ef2e3dcc6c794a8290dcc962b2fe11b335959
vgangotri <virendra.gangotri@genmills.com>
2019-11-18 17:27:37 +0000

    main.tf changes

--- main.tf
+++ main.tf
@@ -3,6 +3,25 @@ resource "azurerm_resource_group" "myresourcegroup" {
   location = "${var.location}"
 }
 
+module "web_app_container" {
+  source              = "app.terraform.io/virendra-sandbox/web-app-container/azurerm"
+  name                = "${var.prefix}"
+  port                = "80"
+  https_only          = "false"
+  resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+  container_type      = "docker"
+  container_image     = "scarolan/palacearcade"
+  plan = {
+    sku_size = "B1"
+    name     = "${var.prefix}-plan"
+  }
+}
+
+output "container_app_url" {
+  value = "http://${module.web_app_container.hostname}"
+}
+
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
   location            = "${azurerm_resource_group.myresourcegroup.location}"
